129fc09c623860c15998f6d4c828d37c760b6a59
Muhammad Reza Irvanda <muzavan@gmail.com>
2015-06-25 14:04:21 +0700

    Tambah Kamus

---
+++ kbbi/assets/js/main.js
@@ -0,0 +1,25 @@
+var kamus;
+
+$(document).ready(function() {
+	$.getJSON("assets/data/kamus.json", function(data) {
+		kamus = data;
+		$('.loader').hide();
+		$('#cari').bind('keypress', function(e) {
+			if (e.keyCode == 13) {
+				var x = kamus[$('#cari').val()]
+				if (x == undefined)
+					x = 'Arti kata tidak ditemukan';
+				$('#arti').text(x);
+			}
+		});
+
+		$('button').bind('click', function(e) {
+			if (e.keyCode == 13) {
+				var x = kamus[$('#cari').val()]
+				if (x == undefined)
+					x = 'Arti kata tidak ditemukan';
+				$('#arti').text(x);
+			}
+		});
+	});
+});
